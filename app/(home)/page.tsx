b1cba2e673de4bcf63f6b12ff4d79df9cc2583c8
Ishrat2413 <ishratrintu2413@gmail.com>
2026-04-04 09:53:17 +0600

    feat(home-page): add Section1 component to home page layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import HomeBanner from "@/components/home/home-page/home-banner";
+import Section1 from "@/components/home/home-page/section-1";
 import ShopeWipes from "@/components/home/home-page/shop-wipes";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <HomeBanner />
       <ShopeWipes />
+      <Section1 />
     </>
   );
 }
---
+++ components/home/home-page/section-1.tsx
@@ -0,0 +1,7 @@
+export default function Section1() {
+  return (
+    <section className='mx-5 md:mx-20 lg:mx-40 md:mt-30'>
+      <p>Section 1</p>
+    </section>
+  );
+}
